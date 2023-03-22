26da79176b1743681b87236f28bd398b0d341e9d
qader qaribiyan <gharibyan.ghader@gmail.com>
2023-03-22 21:00:01 +0100

    Initial commit

---
+++ Note.Test/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Note.Helpers;
+
+namespace Note.Test;
+
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData("",0)]
+    [InlineData("1",1)]
+    [InlineData("1,2",3)]
+    public void Add_AddUpToTwoNumbers_WhenStringISValid(string calculation,int expected)
+
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(calculation);
+        result.Should().Be(expected);
+    }
+}
---
+++ Note/Helpers/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Note.Helpers;
+
+public class Calculator
+{
+    public int Add(string numbersInput)
+    {
+        var parsedNumbers = numbersInput.Split(",");
+        if (!parsedNumbers.Any())
+        {
+            return 0;
+        }
+
+        if (parsedNumbers.Length == 1)
+        {
+            if (parsedNumbers[0].Length == 0)
+            {
+                return 0;
+            }
+
+            return int.Parse(parsedNumbers[0]);
+        }
+        return int.Parse(parsedNumbers[0])+int.Parse(parsedNumbers[1]);
+    }
+}
